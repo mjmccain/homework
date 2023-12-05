0172dd7fbf24dd9b5959b692065d83b1568cf9b3
mjmccain <58721408+mjmccain@users.noreply.github.com>
2023-12-04 18:49:45 -0700

    Add files via upload

--- McCain_Project2.m
+++ McCain_Project2.m
@@ -1,26 +1,28 @@
+%Marcus McCain
+%Project 2
+% This code finds the nth Prime number and graphs it.
+
 %clear command window and workspace
 clc
 clear
 
-
-% Create a randomized input vector and intialize a vector for prime numbers
+% Create a randomized input vector and intialize a vector to store prime numbers
 inputVector = randi([1, 100], 1, 1000);
-
-vectorMax = max(inputVector);
 primesVector = [];
 
-%loop over the random vector and pass each element as input to the function
+%loop over the randomized vector and pass each element as input to the function
+%The functions output is concatenated to primesVector
 for i = 1:length(inputVector)
     primesVector = [primesVector, findPrimes(inputVector(i))];
 end
 
-scatter(inputVector, primesVector, 'o','filled' )
-hold on
-%make a curve fit and plot it
-%trend = fit([inputVector, primesVector], 'exp')
-%plot(trend)
-%use the curve fit to make a prediction for 2025 and plot it
-title('The Nth Prime Numbers')
-xlabel('Integer')
-ylabel('Corresponding Prime')
+%Make a trendline that fits the data, then plot the trendline and data
+[trendline, gof] = fit(inputVector', primesVector', 'poly2')
+plot(trendline,inputVector, primesVector)
+
+%Make the graph look nice. Add title, labels, legend, and grid.
+title('The Nth Prime Numbers',FontSize=16)
+xlabel('Nth-Integer', FontSize=16)
+ylabel('Corresponding Prime', FontSize=16)
+legend('Primes data', 'Trendline', 'Location', 'Northwest',FontSize=16)
 grid on
--- findPrimes.m
+++ findPrimes.m
@@ -1,19 +1,22 @@
-
 function nthPrime = findPrimes(n) 
     primes = 1;
+    i = 3;
     %set a condition for 2, since 2 is the only even prime number
-    if n == 2
-        nthPrime = 1;
+    if n == 1
+        nthPrime = 2;
     else
     %loop over odd numbers and test if they are prime.
-      for i = 1:2:1000
-        if isprime(i)
-           primes = primes + 1;
-           nthPrime = i;
-        end
-        if primes == n
-           break
-        end
+    %looping over only odds makes the code more efficient.
+      for i = 3:1:inf
+          if isprime(i)
+            primes = primes + 1;
+            nthPrime = i;
+          end
+          if primes == n
+             break
+          end
       end
-    end
+    
+      end
+    
 end
